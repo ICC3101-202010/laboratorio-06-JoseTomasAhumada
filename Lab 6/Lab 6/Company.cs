cbe5b22533668ea0c012c3add88c80ab11cb4a73
José Tomás Ahumada <jtahumada@miuandes.cl>
2020-05-17 22:29:26 -0400

    classes added

---
+++ Lab 6/Lab 6/Company.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_6
+{
+    [Serializable]
+    public class Company
+    {
+        private string name;
+        private string rut;
+        private List<Division> l_division;
+        public Company(string name, string rut)
+        {
+            this.name = name;
+            this.rut = rut;
+        }
+    }
+}
--- Lab 6/Lab 6/Program.cs
+++ Lab 6/Lab 6/Program.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +13,26 @@ namespace Lab_6
     {
         static void Main(string[] args)
         {
+            List<Company> company = new List<Company>();
+            Console.WriteLine("¿Desea utilizar un archivo para cargar la información de la empresa? (si/no)");
+            string option = Console.ReadLine();
+            if (option == "no")
+            {
+                Console.WriteLine("Nombre de la empresa:");
+                string name = Console.ReadLine();
+                Console.WriteLine("Rut de la empresa (sin punto ni guión):");
+                string rut = Console.ReadLine();
+                company.Add(new Company(name, rut));
+                Save(company);
+            }
+            Console.ReadKey();
+        }
+        static private void Save(List<Company> company)
+        {
+            IFormatter formatter = new BinaryFormatter();
+            Stream stream = new FileStream("empresa.bin", FileMode.Create, FileAccess.Write, FileShare.None);
+            formatter.Serialize(stream, company);
+            stream.Close();
         }
     }
 }
